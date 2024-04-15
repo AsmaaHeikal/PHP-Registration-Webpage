8b06fd4746ce3e98cac1c04718420ab7b4a6e4b9
saraadel6 <101582885+saraadel6@users.noreply.github.com>
2024-04-15 12:50:12 +0200

    Update DB_Ops.php

--- DB_Ops.php
+++ DB_Ops.php
@@ -27,19 +27,19 @@ class User
 }
 
 // Database class
-Class DBModel
- {
- private $username; 
- private $host;
- private $password; 
- private $database;
- public function __construct()
- {
- $this->username = "root";
- $this->host = "localhost";
- $this->password = "";
- $this->database = "registrationdb";
- }
+class DBModel
+{
+    private $username;
+    private $host;
+    private $password;
+    private $database;
+    public function __construct()
+    {
+        $this->username = "root";
+        $this->host = "localhost";
+        $this->password = "";
+        $this->database = "registrationdb";
+    }
     public function connect()
     {
         $conn = mysqli_connect($this->host, $this->username, $this->password) or die("Couldn't connect to server" . mysqli_error($conn));
@@ -71,24 +71,24 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if (mysqli_num_rows($result) > 0) {
         $msg = "Username already exists! Please choose another username.";
         echo "<script>alert('$msg');</script>";
-    } 
+    }
 
     // The username doesn't exist
     else {
+        if ($pic_error === null) {
+            $user = new User($full_name, $username, $birthdate, $phone_number, $address, $password, $email, $fileNameNew);
 
-        $user = new User($full_name, $username, $birthdate, $phone_number, $address, $password, $email, $fileNameNew);
-
-        // Insert user data into the database
-        $insert_query = "INSERT INTO users (full_name, user_name, password, phone, address, birthdate, email, img_Name)
+            // Insert user data into the database
+            $insert_query = "INSERT INTO users (full_name, user_name, password, phone, address, birthdate, email, img_Name)
             VALUES ('$user->full_name', '$user->username', '$user->password', '$user->phone_number', '$user->address', '$user->birthdate', '$user->email', '$user->img_Name')";
-        
-        if (mysqli_query($conn, $insert_query)) {
-            $msg = "Registration successful!";
-            echo "<script>alert('$msg');</script>";
-        }
-        else {
-            $msg = "Error: " . $insert_query . "<br>" . mysqli_error($conn);
-            echo $msg;
+
+            if (mysqli_query($conn, $insert_query)) {
+                $msg = "Registration successful!";
+                echo "<script>alert('$msg');</script>";
+            } else {
+                $msg = "Error: " . $insert_query . "<br>" . mysqli_error($conn);
+                echo $msg;
+            }
         }
     }
 }
